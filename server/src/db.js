95f604d29b7fc1cadb09d5f4a3ae1b56e89cf057
M Taha Makhdoom <taha.makhdoom10@gmail.com>
2024-05-28 19:18:58 +0500

    credentials altered v2

--- server/src/db.js
+++ server/src/db.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 
 let dbConnection;
 
-const username = "tahamakhdoom10";
-const password = "XLvZtgcIspiBIgei";
+const username = process.env.USER;
+const password = process.env.PASSWORD;
 
-const dbURL = `mongodb+srv://${username}:${password}@inventorymgmtsystem.ihafze5.mongodb.net/`;
+const dbURL = process.env.MONGODB_URI | `mongodb+srv://${username}:${password}@inventorymgmtsystem.ihafze5.mongodb.net/`;
+
 
 module.exports = {
     connectToDb:(cb) => {
