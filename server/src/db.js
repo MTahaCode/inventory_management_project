e7a72dc7b39fc788f0bc168a9df84ff1eb598844
M Taha Makhdoom <taha.makhdoom10@gmail.com>
2024-05-28 19:33:56 +0500

    changed bitwise or to normal one

--- server/src/db.js
+++ server/src/db.js
@@ -6,7 +6,7 @@ let dbConnection;
 const username = process.env.USER;
 const password = process.env.PASSWORD;
 
-const dbURL = process.env.MONGODB_URI | `mongodb+srv://${username}:${password}@inventorymgmtsystem.ihafze5.mongodb.net/`;
+const dbURL = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@inventorymgmtsystem.ihafze5.mongodb.net/`;
 
 
 module.exports = {
